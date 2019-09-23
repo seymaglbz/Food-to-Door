b3cf9851bc49c1ebedf881fa4a7049ac18485916
Seyma Gilbaz <seymagilbaz@gmail.com>
2019-09-23 09:50:20 +0300

    Initial Commit

---
+++ Food to Door/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food to Door
+//
+//  Created by Şeyma Gılbaz on 23.09.2019.
+//  Copyright © 2019 Şeyma Gılbaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
